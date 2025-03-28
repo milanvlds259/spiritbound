fced6ec37d9181fdd4d8624bba0fb14f9a65b88e
Logan Gier <logan.gier@gmail.com>
2025-03-28 11:18:11 -0500

    Minotaur updates

--- Scenes/Bosses/bossminotaur.gd
+++ Scenes/Bosses/bossminotaur.gd
@@ -1,6 +1,6 @@
 class_name Minotaur extends CharacterBody2D
 
-# Speed, Health, Damage#
+# Speed, Health, Damage #
 @export var speed: float = 100
 var charge_speed: float
 @export var max_health: int = 1
@@ -18,7 +18,7 @@ var hurt_duration = 0.1
 @onready var hit_box: Area2D = $HitBox         # The Area2D used for taking damage
 
 # Attacking #
-@onready var attacking = false
+@onready var is_attacking = false
 @onready var charging = false
 @export var impulse_str: float = 500
 @onready var attack_range: Area2D = $AttackRange   # The Area2D for checking if player is in range
@@ -45,6 +45,10 @@ func _ready() -> void:
 	current_health = max_health
 	charging_position = Vector2.ZERO
 
+
+	$AttackEffect/AttackSprite.frame_changed.connect(_on_attack_effect_frame_changed)
+	$AttackEffect/AttackHitbox.disabled = true
+	
 	if hit_box:
 		# Connect enemy hitbox when area entered
 		hit_box.area_entered.connect(_on_hitbox_area_entered)
@@ -52,7 +56,7 @@ func _ready() -> void:
 		hit_box.body_entered.connect(_on_hitbox_body_entered)
 	
 	# Initialize base guarding spot.
-	start_position = global_position
+	start_position = position
 
 
 func _process(_delta: float) -> void:
@@ -68,14 +72,19 @@ func updateAnimations() -> void:
 		###########################CHANGE ANIMATIONS WHEN COMPLETE
 		State.IDLE:
 			$AnimatedSprite2D.play("walk") # play idle animation
+			modulate = Color(1, 1, 1, 1)
 		State.FOLLOW:
 			$AnimatedSprite2D.play("walk") # play walking animation
+			modulate = Color(1, 1, 1, 1)
 		State.BACK:
 			$AnimatedSprite2D.play("walk") # play walking animation (walking back to spot)
+			modulate = Color(1, 1, 1, 1)
 		State.ATTACK:
 			$AnimatedSprite2D.play("walk") # play attacking animation
+			modulate = Color(1, 1, 0, 1)
 		State.CHARGE:
 			$AnimatedSprite2D.play("walk") # play charging animation
+			modulate = Color(1, 0, 1, 1)
 
 	# Orients the sprite based on the direction of movement
 	if velocity.x > 0:
@@ -105,11 +114,15 @@ func update_velocity() -> void:
 					velocity = new_velocity
 				
 				State.ATTACK:
+					print("attacking")
 					velocity = Vector2.ZERO
 				
 				State.BACK:
+					print("going back")
 					var dir_to_start = start_position - global_position
+					print(dir_to_start)
 					if dir_to_start.length() < limit:
+						print("reset")
 						position = start_position
 						velocity = Vector2.ZERO
 						current_state = State.IDLE
@@ -123,11 +136,13 @@ func update_velocity() -> void:
 		Mind.ANNOYED:
 			match current_state:
 				State.IDLE:
+					print("idle")
 					var overlap = follow_area.get_overlapping_bodies()
 					var filtered = overlap.filter(func(b): return b is Player)
 					if !filtered.is_empty():
 						follow_body(filtered[0])
 				State.FOLLOW:
+					print("follow")
 					check_attack();
 					var overlap = follow_area.get_overlapping_bodies()
 					var filtered = overlap.filter(func(b): return b is Player)
@@ -135,11 +150,13 @@ func update_velocity() -> void:
 						current_state = State.BACK
 						return
 					var direction = target.global_position - global_position
-					var new_velocity = direction.normalized() * speed
+					var new_velocity = direction.normalized() * speed * 1.5
 					velocity = new_velocity
 				State.ATTACK:
+					print("attack")
 					velocity = Vector2.ZERO
 				State.BACK:
+					print("back")
 					var dir_to_start = start_position - global_position
 					if dir_to_start.length() < limit:
 						position = start_position
@@ -156,11 +173,13 @@ func update_velocity() -> void:
 		Mind.ANGERED:
 			match current_state:
 				State.IDLE:
+					print("idle")
 					var overlap = vision.get_overlapping_bodies()
 					var filtered = overlap.filter(func(b): return b is Player)
 					if !filtered.is_empty():
 						follow_body(filtered[0])
 				State.FOLLOW:
+					print("follow")
 					check_attack();
 					var over_vis = vision.get_overlapping_bodies()
 					var filt_vis = over_vis.filter(func(b): return b is Player)
@@ -173,15 +192,17 @@ func update_velocity() -> void:
 						charge(filt_vis[0])
 						return
 					var direction = target.global_position - global_position
-					var new_velocity = direction.normalized() * speed
+					var new_velocity = direction.normalized() * speed * 2
 					velocity = new_velocity
 				State.ATTACK:
+					print("attack")
 					velocity = Vector2.ZERO
 				State.CHARGE:
+					print("charge")
 					if !charging:
 						return
 					var dir_to_charge = charging_position - global_position
-					if dir_to_charge.length() < limit*2:
+					if dir_to_charge.length() < limit*3:
 						position = charging_position
 						velocity = Vector2.ZERO
 						current_state = State.IDLE
@@ -189,8 +210,9 @@ func update_velocity() -> void:
 						charging_position = global_position
 						await get_tree().create_timer(1).timeout
 						return
-					velocity = dir_to_charge.normalized() * 3 * speed
+					velocity = dir_to_charge.normalized() * charge_speed
 				State.BACK:
+					print("back")
 					var dir_to_start = start_position - global_position
 					if dir_to_start.length() < limit:
 						position = start_position
@@ -204,38 +226,24 @@ func update_velocity() -> void:
 					velocity = dir_to_start.normalized() * speed
 
 func check_attack() -> void:
-	var overlap = attack_range.get_overlapping_bodies()
-	var filtered = overlap.filter(func(b): return b is Player)
-	if !filtered.is_empty():
-		attack_melee(filtered[0])
-		current_state = State.ATTACK
-	await get_tree().create_timer(1).timeout
-	current_state = State.IDLE
+	var attack_space = attack_range.get_overlapping_bodies()
+	var attack_target = attack_space.filter(func(b): return b is Player)
+	if !attack_target.is_empty():
+		if !is_attacking:
+			is_attacking = true
+			attack_melee(attack_target[0])
+			current_state = State.ATTACK
+			return
 
 func follow_body(body) -> void:
 	target = body
 	current_state = State.FOLLOW
-	print("chasing")
 
 func charge(body) -> void:
 	target = body
 	current_state = State.CHARGE
-	charging_position = global_position
-	await get_tree().create_timer(1).timeout
 	charging_position = body.global_position
 	charging = true
-
-#func stop_follow() -> void:
-#	if current_state != State.BACK:
-#		target = null
-#		print("timeout")
-#		current_state = State.BACK
-
-func _change_marker_direction() -> void:
-	# Swap start and end positions.
-	var temp: Vector2 = end_position
-	end_position = start_position
-	start_position = temp
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	# Check if the area is the player's attack hitbox (temporary)
@@ -249,36 +257,38 @@ func _on_hitbox_body_entered(body: Node) -> void:
 
 	var push_dir = (body.global_position - global_position).normalized()
 	if body.has_method("apply_knockback"):
-		body.apply_knockback(push_dir * impulse_str)
-		await get_tree().create_timer(0.5).timeout
+		body.apply_knockback(push_dir * 2* impulse_str)
 
 func attack_melee(body) -> void:
-	attacking = true
 	if body.is_in_group("player"):
-		if body.has_method("stun"):
-			body.stun(0.75)
 		var push_dir = (body.global_position - global_position).normalized()
 		var angle = push_dir.angle();
-		
-		if body.has_method("apply_knockback"):
-			await get_tree().create_timer(0.5).timeout
-			body.apply_knockback(push_dir * impulse_str)
+		await get_tree().create_timer(0.5).timeout
+
+
 		if $AttackEffect:
 			$AttackEffect.rotation = angle
 			$AttackEffect.position = push_dir * 5
 			match mind_state:
 				Mind.CALM:
 					$AttackEffect/AttackSprite.play("attack")
-					body.take_damage(base_damage)
 
 				Mind.ANNOYED:
 					$AttackEffect/AttackSprite.play("attack")
-					body.take_damage(2*base_damage)
 
 				Mind.ANGERED:
 					$AttackEffect/AttackSprite.play("attack_fire")
-					body.take_damage(3*base_damage)
 
+func _on_attack_effect_frame_changed():
+	var current_frame = $AttackEffect/AttackSprite.frame
+	# If the current frame is where the attack should be active.
+	# Adjust the frame numbers (e.g. 3 and 4) as needed depending on your animation indexing.
+	if mind_state == Mind.ANGERED and current_frame == 5:
+		$AttackEffect/AttackHitbox.disabled = false
+	elif current_frame == 3 or current_frame == 4:
+		$AttackEffect/AttackHitbox.disabled = false
+	else:
+		$AttackEffect/AttackHitbox.disabled = true
 
 
 
@@ -336,3 +346,29 @@ func died() -> void:
 func disable() -> void:
 	# Disable further physics processing (could also play a death animation, etc.).
 	set_physics_process(false)
+
+func _on_attack_effect_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		match mind_state:
+			Mind.CALM:
+				body.stun(0.25)
+				body.apply_knockback(body.position-global_position)
+				body.take_damage(base_damage)
+
+			Mind.ANNOYED:
+				body.stun(0.5)
+				body.apply_knockback(body.position-global_position)
+				body.take_damage(2*base_damage)
+
+			Mind.ANGERED:
+				body.stun(0.75)
+				body.apply_knockback(body.position-global_position)
+				body.take_damage(3*base_damage)
+		
+	 # Replace with function body.
+
+
+func _on_attack_sprite_animation_finished() -> void:
+	is_attacking = false
+	current_state = State.FOLLOW
+	 # Replace with function body.
