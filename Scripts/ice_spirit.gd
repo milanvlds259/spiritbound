5ce359f6ddf81daa57a8286fff0c0650cbb6dbaf
Logan Gier <logan.gier@gmail.com>
2025-04-13 20:40:41 -0500

    Greatsword Skeleton added + Fixed bug with showing next button for player

--- Scripts/ice_spirit.gd
+++ Scripts/ice_spirit.gd
@@ -4,14 +4,14 @@ class_name IceSpirit
 signal spirit_picked_up
 
 func _ready() -> void:
-    body_entered.connect(_on_body_entered)
-    $AnimatedSprite2D.play("idle")
+	body_entered.connect(_on_body_entered)
+	$AnimatedSprite2D.play("idle")
 
 func _process(delta: float) -> void:
-    pass
+	pass
 
 func _on_body_entered(body: Node) -> void:
-    if body.is_in_group("player"):
-        body.add_spirit("ice")
-        Global.emit_signal("spirit_picked_up")
-        queue_free()
+	if body.is_in_group("player"):
+		body.add_spirit("ice")
+		Global.emit_signal("spirit_picked_up")
+		queue_free()
