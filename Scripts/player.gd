845cb0a0ff0e242306a91fe229cc5d6eb446904b
genghis-cpu <rajamani@utexas.edu>
2025-04-16 14:41:30 -0500

    Rahul (#35)
    * 2 more sounds
    
    * changes
    
    * changes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -239,6 +239,7 @@ func _attack_ranged():
 	attack_mode = "ranged"
 	arrow_fired = false
 	$PlayerSprite.play("attack_ranged")
+	$PlayerRangedHit.play()
 
 func fire_arrow():
 	var used_arrow: PackedScene
@@ -319,6 +320,7 @@ func take_damage(damage: int):
 		die()
 	Global.emit_signal("player_hp_changed", hp)
 	$PlayerSprite.play("hurt")
+	$PlayerHurt.play()
 
 
 func die():
